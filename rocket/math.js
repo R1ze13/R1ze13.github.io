a16cf79a066b6ca2290074b295e3212f9b4c28ce
R1ze13 <justdieplz13@gmail.com>
2016-08-07 15:08:31 +0300

    refactoring math.js. fixed bug with first input's math

--- rocket/math.js
+++ rocket/math.js
@@ -1,21 +1,47 @@
+$(document).ready(function() {
+	
+	
+var diameter, tensileForce, radius, area, tension;
+
+function getValues() {
+	diameter = $('#diameter').val();
+	tensileForce = $('#tensileForce').val();
+	radius = diameter / 2;
+	return true;
+}
+
 function areaCalc() {
-	var diameter = $('#diameter').val();
-	var tensileStrength = $('#tensileStrength').val();
-	var radius = diameter / 2;
-	var area = Math.PI * radius * radius;
+	getValues();
+	area = Math.PI * radius * radius;
 	if (area > 0) {
 		$('#area').html('S = ' + area.toFixed(1) + " mm<sup>2</sup>");
 	}
 	else $('#area').empty();
 	return area;
-};
+}
 
 function tensionCalc() {
-	var tensileStrength = $('#tensileStrength').val();
-	var tension = tensileStrength / areaCalc();
-	if (areaCalc() !== 0 && areaCalc() !== undefined && tension !== Infinity && tension > 0) {
-		$('#tension').val(tension);
+	getValues();
+	if (area > 0) {
+		tension = tensileForce / area;
+	}
+	return tension;
+}
+
+function calcMain() {
+	areaCalc();
+	tensionCalc();
+	if (area > 0 && tension > 0) {
+		$('#tension').val(tension.toFixed(2));
 	}
 	else $('#tension').val('').attr('placeholder', 'Tension');
-	return tension;
-};
+}
+	
+	$('#diameter').keyup(function() {
+		calcMain();
+	});
+	$('#tensileForce').keyup(function() {
+		calcMain();
+	});
+	
+});
