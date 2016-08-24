2d77f3c2cd18da456fbd9678b514591d286f02be
R1ze13 <justdieplz13@gmail.com>
2016-08-24 17:44:04 +0300

    added left menu

--- rocket/js/script.js
+++ rocket/js/script.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
-  $('.login').find('p').click(function() {
+	// LOGIN
+	$('.login').find('p').click(function() {
     $('.login').find('.dropdown-menu').fadeToggle("fast", "linear");
   });
   
 	$('.kindOfPipe-group').find('.dropdown-toggle').click(function() {
 		$('.kindOfPipe-group').find('.dropdown-menu').fadeToggle(200);
 	});
+	// /LOGIN
 	
+	// ACCORDION
   $( "#accordion" ).accordion({
 		heightStyle: "content",
 		collapsible: true
 	});
+	// /ACCORDION
+	
+		// MENU
+	$('.icon-menu').click(function() {
+		$('.menu').animate({
+			left: '0px'    
+		}, 300);
+		$('.header').find('.icon-menu').fadeOut(300);
+	});
+	
+	$('.icon-close').click(function() {
+		$('.menu').animate({
+			left: '-285px'
+		}, 300);
+		$('.header').find('.icon-menu').fadeIn(300);
+	});
+	// /MENU
 });
