3997c94682e3d293944b6709da8816fa6e070610
R1ze13 <justdieplz13@gmail.com>
2016-10-10 21:59:10 +0300

    added teleport

--- snake/js/snake.js
+++ snake/js/snake.js
@@ -1,67 +1,93 @@
 $(document).ready(function() {
 	
 // Canvas
-	var canvas = $('#canvas')[0];
-	var ctx = canvas.getContext("2d");
+	var canvas = $('#canvas')[0],
+			ctx = canvas.getContext("2d");
 	canvas.width = 600;
 	canvas.height = 600;
-	var width = $("#canvas").width();
-	var height = $("#canvas").height();
-	var cellWidth = 15;
-	var direction = 'right';
-	var snakeArray;
-	var snakeLength = 4;
+	
+	var width = $("#canvas").width(),
+			height = $("#canvas").height(), 
+			cellWidth = 15, 
+			direction = 'right', 
+			snakeArray,
+			snakeLength = 4,
+			food;
+	
+	var drawTimer,
+			foodTimer;
+	
+	init();
+	function init() {
+		createField();
+		createSnake();
+		draw();
+//		foodTimer = setInterval(createFood, 2000);
+	};
 	
 	function createSnake() {
 		snakeArray = [];
 		
-		for (var i = snakeLength - 1; i >= 0; i--) {
+		for (var i = snakeLength; i >= 0; i--) {
 			snakeArray.push({x: i+13, y: 18});
 		}
 	}
 	
-	function draw() {
+	function createFood() {
+		food = {
+			x: Math.round(Math.random()*(width / cellWidth)),
+			y: Math.round(Math.random()*(height / cellWidth))
+		};
+		
+		ctx.fillStyle = 'yellow';
+		ctx.fillRect(food.x * cellWidth, food.y*cellWidth, cellWidth - 1, cellWidth - 1);
+		ctx.strokeStyle = 'orange';
+		ctx.strokeRect(food.x * cellWidth, food.y*cellWidth, cellWidth - 1, cellWidth - 1);
+	}
+	
+	function createField() {
 		// field
 		ctx.fillStyle = 'rgba(28, 28, 28, 1)';
 		ctx.fillRect(0, 0, width, height);
-		//	border
-//		ctx.strokeStyle = '#000';
-//		ctx.strokeRect(0, 0, width, height);
-		
+	}
+	
+	function draw() {
 		//	snake movement
 		var headX = snakeArray[0].x;
 		var headY = snakeArray[0].y;
-//		headX++;
 		
 		//	snake turns && movement
 		if (direction === 'right') headX++;
 		else if (direction === 'down') headY++;
 		else if (direction === 'left') headX--;
 		else if (direction === 'up') headY--;
-		
 		var tail = snakeArray.pop();
 		tail.x = headX;
 		tail.y = headY;
 		snakeArray.unshift(tail);
 		
-		//	snake turns
-//		if (direction === 'right') headX++;
-//		else if (direction === 'down') headY--;
-//		else if (direction === 'left') headX--;
-//		else if (direction === 'up') headY++;
+		//	teleport
+		if (headX === width/cellWidth) snakeArray[0].x = 0;
+		if (headX === -1) snakeArray[0].x = width/cellWidth - 1;
+		if (headY === height/cellWidth) snakeArray[0].y = 0;
+		if (headY === -1) snakeArray[0].y = height/cellWidth - 1;
 		
 		//	snake
 		for (var i = 0; i < snakeArray.length; i++) {
 			var snake = snakeArray[i];
 			ctx.fillStyle = "rgba(181, 181, 181, 0.72)";
 			ctx.fillRect(snake.x * cellWidth, snake.y * cellWidth, cellWidth, cellWidth);
-			ctx.strokeStyle = "#fff";
-			ctx.strokeRect(snake.x * cellWidth, snake.y * cellWidth, cellWidth, cellWidth);
+//			ctx.strokeStyle = "#fff";
+//			ctx.strokeRect(snake.x * cellWidth, snake.y * cellWidth, cellWidth - 1, cellWidth - 1);
 		}
+		
+		
+		// painting place under the tale
+		ctx.fillStyle = 'rgba(28, 28, 28, 1)';
+		ctx.fillRect( snakeArray[snakeArray.length - 1].x * cellWidth, snakeArray[snakeArray.length - 1].y * cellWidth, cellWidth + 1, cellWidth + 1 );
+//		ctx.strokeStyle = 'rgba(28, 28, 28, 1)';
+//		ctx.strokeRect( snakeArray[snakeArray.length - 1].x * cellWidth, snakeArray[snakeArray.length - 1].y * cellWidth, cellWidth, cellWidth );
 	};
-	
-	createSnake();
-	var drawTimer = setInterval(draw, 90);
 	
 	//	buttons
 	$('#game-wrapper__btn-start').click(function() {
@@ -76,7 +102,7 @@ $(document).ready(function() {
 	});
 	
 	$('#game-wrapper__btn-reset').click(function() {
-		createSnake();
+		init();
 		if(typeof drawTimer == "undefined") {
 			drawTimer = setInterval(draw, 90);
 		}
