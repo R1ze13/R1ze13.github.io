f4670d9cf4003a80e31ef00c491a6e19f8c2b85a
R1ze13 <justdieplz13@gmail.com>
2016-10-27 03:27:47 +0300

    added options

--- snake/js/snake.js
+++ snake/js/snake.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
 			score = 0,
 			speed = 150,
 			isRotateAllowed = true,
+			isOptionsAllowed = true,
 			gameOver = false;
 	
 	let drawTimer,
@@ -25,17 +26,30 @@ $(document).ready(function() {
 	let headX, headY;
 	
 	$('.canvas-wrapper').css({
-		'height': '600px',
-		'width': '600px'
+		'height': height + 'px',
+		'width': width + 'px'
 	});
 	
 	init();
 	function init() {
-		gameOver = false;
+		getValues();
 		createField();
 		createSnake();
 		createFood();
 		draw();
+	}
+	
+	function getValues() {
+		if (isOptionsAllowed === false) return;
+		let speedInput = +$('.options__speed-input').val();
+		if ( speedInput !== undefined && speedInput !== 0 ) {
+			speed = speedInput;
+		}
+		
+		let snakeLengthInput = +$('.options__snake-length-input').val();
+		if ( snakeLengthInput !== 0 ) {
+			snakeLength = snakeLengthInput;
+		}
 	}
 	
 	function createSnake() {
@@ -47,8 +61,8 @@ $(document).ready(function() {
 	
 	function createFood() {
 		food = {
-			x: Math.round(Math.random()*( (width - cellWidth) / cellWidth)),
-			y: Math.round(Math.random()*( (height - cellWidth) / cellWidth))
+			x: Math.round(Math.random()*( (width - cellWidth) / cellWidth )),
+			y: Math.round(Math.random()*( (height - cellWidth) / cellWidth ))
 		};
 		for (let i = 0; i < snakeArray.length; i++) {
 			if (snakeArray[i].x === food.x && snakeArray[i].y === food.y) createFood();
@@ -57,9 +71,9 @@ $(document).ready(function() {
 	
 	function dropFood() {
 		ctx.fillStyle = 'yellow';
-		ctx.fillRect(food.x * cellWidth, food.y*cellWidth, cellWidth - 1, cellWidth - 1);
+		ctx.fillRect(food.x * cellWidth, food.y*cellWidth, cellWidth, cellWidth);
 		ctx.strokeStyle = 'orange';
-		ctx.strokeRect(food.x * cellWidth, food.y*cellWidth, cellWidth - 1, cellWidth - 1);
+		ctx.strokeRect(food.x * cellWidth, food.y*cellWidth, cellWidth, cellWidth);
 	}
 	
 	function eatFood() {
@@ -84,11 +98,8 @@ $(document).ready(function() {
 		ctx.fillRect(0, 0, width, height);
 	}
 	
-//	checkCollision();
 	function checkCollision() {
-		//	snake movement
-		headX = snakeArray[0].x;
-		headY = snakeArray[0].y;
+		if (isOptionsAllowed === true) return;
 		for (let i = 1; i < snakeArray.length; i++) {
 			if (headX === snakeArray[i].x && headY === snakeArray[i].y) {
 				clearInterval(drawTimer);
@@ -102,35 +113,34 @@ $(document).ready(function() {
 	}
 	
 	function reset() {
-//		headX = undefined;
-//		headY = undefined;
 		speed = 150;
 		score = 0;
+		gameOver = false;
 		$('.canvas-wrapper').addClass('change').attr('data-content', 'score: ' + score);
 		direction = 'right';
 		$('.game-overlay').stop().fadeOut('linear');
-		if(typeof drawTimer == "undefined") {
-			drawTimer = setInterval(draw, speed);
-		}
 	}
 	
 	function draw() {
 		createField();
 		checkCollision();
-		if (gameOver === true) return;
 		dropFood();
+		headX = snakeArray[0].x;
+		headY = snakeArray[0].y;
 		eatFood();
 		
 		//	snake turns && movement
-		if (direction === 'right') headX++;
-		else if (direction === 'down') headY++;
-		else if (direction === 'left') headX--;
-		else if (direction === 'up') headY--;
-		let tail = snakeArray.pop();
-		tail.x = headX;
-		tail.y = headY;
-		snakeArray.unshift(tail);
-		isRotateAllowed = true;
+		if (gameOver === false) {
+			if (direction === 'right') headX++;
+			else if (direction === 'down') headY++;
+			else if (direction === 'left') headX--;
+			else if (direction === 'up') headY--;
+			let tail = snakeArray.pop();
+			tail.x = headX;
+			tail.y = headY;
+			snakeArray.unshift(tail);
+			isRotateAllowed = true;
+		}
 		
 		//	teleport
 		if (headX === width/cellWidth) snakeArray[0].x = 0;
@@ -148,20 +158,33 @@ $(document).ready(function() {
 		}
 	}
 	
+	//	options
+	$('.options__options-input').keyup(function() {
+		if (isOptionsAllowed === false) return;
+		getValues();
+		createSnake();
+		draw();
+	});
+	
 	//	buttons
+	//	start button
 	$('#game-wrapper__btn-start').click(function() {
+		isOptionsAllowed = false;
+		$('.options__options-input').attr('disabled', true);
+		
 		if (gameOver === false) {
-			if(typeof drawTimer == "undefined") {
-				drawTimer = setInterval(draw, speed);
-			}
 			$('.game-overlay').stop().fadeOut('linear');
 		}
 		else {
 			reset();
 			init();
 		}
+		if(typeof drawTimer == "undefined") {
+				drawTimer = setInterval(draw, speed);
+		}
 	});
 	
+	//	pause button
 	$('#game-wrapper__btn-pause').click(function() {
 		clearInterval(drawTimer);
 		drawTimer = undefined;
@@ -171,7 +194,13 @@ $(document).ready(function() {
 		$('.game-overlay').find('p').text('Score: ' + score);
 	});
 	
+	//	reset button
 	$('#game-wrapper__btn-reset').click(function() {
+		$('.options__options-input').attr('disabled', false);
+		isOptionsAllowed = true;
+		clearInterval(drawTimer);
+		drawTimer = undefined;
+		gameOver = true;
 		reset();
 		init();
 	});
