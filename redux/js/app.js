c85ed1d07010ca94506628bd03310904accc61f3
R1ze13 <justdieplz13@gmail.com>
2017-01-12 20:35:27 +0300

    tablet redux v.1.0 is ready

--- redux/js/app.js
+++ redux/js/app.js
@@ -1,24 +1,40 @@
-$(document).ready(function() {
-	
-	$('.arrows__left').click(function() {
+$(document).ready(function () {
+
+	$('.arrows__left').click(function () {
 		var mS = $('.slider__middleSlide').attr('src');
 		var rS = $('.slider__rightSlide').attr('src');
 		var lS = $('.slider__leftSlide').attr('src');
-		
+
 
 		$('.slider__middleSlide').attr('src', lS);
 		$('.slider__rightSlide').attr('src', mS);
 		$('.slider__leftSlide').attr('src', rS);
 	});
-	
-	$('.arrows__right').click(function() {
+
+	$('.arrows__right').click(function () {
 		var mS = $('.slider__middleSlide').attr('src');
 		var rS = $('.slider__rightSlide').attr('src');
 		var lS = $('.slider__leftSlide').attr('src');
-		
+
 		$('.slider__middleSlide').attr('src', rS);
 		$('.slider__rightSlide').attr('src', lS);
 		$('.slider__leftSlide').attr('src', mS);
 	});
+
+
+	function setEqualHeight(columns) {
+		var tallestcolumn = 0;
+		columns.each(
+			function () {
+				currentHeight = $(this).height();
+				if (currentHeight > tallestcolumn) {
+					tallestcolumn = currentHeight;
+				}
+			}
+		);
+		columns.height(tallestcolumn);
+	}
+	setEqualHeight($(".equal-height-for-tablet"));
 	
+	$('.hide-long-text').readmore();
 });
