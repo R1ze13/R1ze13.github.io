46e2d03d7de3cafbcd5b60de9455a58923a4c0d7
R1ze13 <justdieplz13@gmail.com>
2017-01-14 00:43:46 +0300

    added checking entered email

--- redux/js/app.js
+++ redux/js/app.js
@@ -36,10 +36,39 @@ $(document).ready(function () {
 		columns.height(tallestColumn);
 	}
 	setEqualHeight($(".equal-height-for-tablet"));
-	
-	$(window).resize(function() {
+
+	$(window).resize(function () {
 		setEqualHeight($(".equal-height-for-tablet"));
 	});
-	
+
 	$('.hide-long-text').readmore();
+
+
+	$('.newsletter__subscribe').click(function () {
+		if ($('.newsletter__email-input').val() != '') {
+			var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+			if (pattern.test($('.newsletter__email-input').val())) {
+				$('.newsletter__check-wrapper').addClass('newsletter__check-wrapper-active');
+				$('.newsletter__answer-icon')
+					.removeClass('newsletter__answer-icon-cross')
+					.addClass('newsletter__answer-icon-tick');
+				$('.newsletter__check')
+					.removeClass('newsletter__check-not-okay')
+					.addClass('newsletter__check-okay')
+					.text('thanks, your address has been added');
+			} else {
+				$('.newsletter__check-wrapper').addClass('newsletter__check-wrapper-active');
+				$('.newsletter__answer-icon')
+					.removeClass('newsletter__answer-icon-tick')
+					.addClass('newsletter__answer-icon-cross');
+				$('.newsletter__check')
+					.removeClass('newsletter__check-okay')
+					.addClass('newsletter__check-not-okay')
+					.text('please enter a valid email address');
+			}
+		}
+		setTimeout(function() {
+			$('.newsletter__check-wrapper').removeClass('newsletter__check-wrapper-active');
+		}, 5000);
+	});
 });
