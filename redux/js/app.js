44a78bde79604dc05e0f254f108e4f0da64bebcf
R1ze13 <justdieplz13@gmail.com>
2017-01-19 21:35:38 +0300

    added dotdotdot. and fix head menus again

--- redux/js/app.js
+++ redux/js/app.js
@@ -26,7 +26,6 @@ $(document).ready(function () {
 	// dropdown-menu
 	$('.dropdown-toggle').click(function() {
 		$('.dropdown-menu').not($(this).children()).removeClass('dropdown-menu-active');
-		console.log($(this).children());
 		$('.dropdown-toggle').not(this).removeClass('nav__item-active');
 		$(this).toggleClass('nav__item-active');
 		$(this).children('.dropdown-menu').toggleClass('dropdown-menu-active');
@@ -34,7 +33,7 @@ $(document).ready(function () {
 	// /dropdown-menu
 
 	function setEqualHeight(columns) {
-		$('.equal-height-for-tablet').height('auto');
+		$('.equal-height-of-columns').height('auto');
 		var tallestColumn = 0;
 		var currentHeight = 0;
 		columns.each(
@@ -47,10 +46,11 @@ $(document).ready(function () {
 		);
 		columns.height(tallestColumn);
 	}
-	setEqualHeight($(".equal-height-for-tablet"));
+	setEqualHeight($(".equal-height-of-columns"));
 
 	$(window).resize(function () {
-		setEqualHeight($(".equal-height-for-tablet"));
+		setEqualHeight($(".equal-height-of-columns"));
+		$('.max-lines').trigger("update.dot");
 	});
 
 //	$('.hide-long-text').readmore();
@@ -92,12 +92,14 @@ $(document).ready(function () {
 		//	dropdown-menu
 		(function() {
 			if ($(event.target).hasClass('dropdown-menu')) return;
+			if ($(event.target).hasClass('nav__item')) return;
 			if ($(event.target).closest(".dropdown-toggle").length) return;
-			$('.dropdown-menu').stop().fadeOut("fast", "linear");
 			$('.dropdown-menu-active').removeClass('dropdown-menu-active');
 			$('.nav__item-active').removeClass('nav__item-active');
 		})();
 		// /dropdown-menu
 	});
 	// /События по клику мимо
+	
+	$('.max-lines').dotdotdot();
 });
